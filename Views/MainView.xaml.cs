f10a0fc78b442f90109271656adc82f0240f2f5d
cwalker <cwalker@oncology.local>
2020-08-12 09:28:51 -0700

    Add project files.

---
+++ Views/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace RT_Checker.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
